08cdcd92db98e1815cf8972457add89e91d7dc49
Vishal580 <83541548+Vishal580@users.noreply.github.com>
2024-07-30 13:55:41 +0530

    Major Project

---
+++ Mouse_Cursor_Control/head_pose.py
@@ -0,0 +1,103 @@
+import numpy as np
+import cv2
+import dlib
+from imutils import face_utils
+import imutils
+
+
+# Return direction given the nose and anchor points.
+def direction(nose_point, anchor_point, w, h, multiple=1):
+    nx, ny = nose_point
+    x, y = anchor_point
+
+    if nx > x + multiple * w:
+        return 'right'
+    elif nx < x - multiple * w:
+        return 'left'
+
+    if ny > y + multiple * h:
+        return 'down'
+    elif ny < y - multiple * h:
+        return 'up'
+
+    return '-'
+
+
+
+INPUT_MODE = True
+ANCHOR_POINT = (252, 310)
+shape_predictor = "model/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(shape_predictor)
+
+
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+
+vid = cv2.VideoCapture(0)
+resolution_w = 1366
+resolution_h = 768
+cam_w = 640
+cam_h = 480
+unit_w = resolution_w / cam_w
+unit_h = resolution_h / cam_h
+
+
+while True:
+    # Grab the frame from the threaded video file stream, resize
+    # it, and convert it to grayscale
+    # channels)
+    _, frame = vid.read()
+    frame = cv2.flip(frame, 1)
+    frame = imutils.resize(frame, width=cam_w, height=cam_h)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    rects = detector(gray, 0)
+
+    # Loop over the face detections
+    if len(rects) > 0:
+        rect = rects[0]
+    else:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        continue
+
+    # Determine the facial landmarks for the face region, then
+    # convert the facial landmark (x, y)-coordinates to a NumPy
+    # array
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+    
+    nose = shape[nStart:nEnd]
+    nose_point = (nose[3, 0], nose[3, 1])
+    #ANCHOR_POINT = nose_point
+    #print(nose_point)
+
+    if INPUT_MODE:
+        cv2.putText(frame, "CURSOR STARTED!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        x, y = ANCHOR_POINT
+        nx, ny = nose_point
+        w, h = 60, 35
+        multiple = 1
+        cv2.rectangle(frame, (x - w, y - h), (x + w, y + h), (0,255,0), 2)
+        cv2.line(frame, ANCHOR_POINT, nose_point, (255,0,0), 2)
+
+        dir = direction(nose_point, ANCHOR_POINT, w, h)
+        cv2.putText(frame, dir.upper(), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        print(dir)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # If the `Esc` key was pressed, break from the loop
+    if key == 27:
+        break
+
+# Do a bit of cleanup
+cv2.destroyAllWindows()
+vid.release()
+
+
+
+
+
